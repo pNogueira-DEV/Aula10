46ab126cf964d97691e7abc1db36a336632d3dc9
paulo <pauloricardo.st1812@hotmai.com>
2026-03-20 16:22:49 -0300

    Codigo completo e correto

--- main02.py
+++ main02.py
@@ -1,23 +1,22 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
+from sqlalchemy import create_engine, Column, Integer, Float, String, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
-
 
 Base = declarative_base()
 
-
 class Departamento(Base):
     __tablename__ = "departamentos"
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String(100), nullable=False)
 
+    funcionarios = relationship("Funcionario", back_populates="departamento")
 
     def __init__(self, nome):
         self.nome = nome
 
+    #Função para imprimir
     def __repr__(self):
-        return f"Usuario = id={self.id} - nome={self.nome}"
-    
-
+        return f"Departamento =  id={self.id} - nome={self.nome}"
 
 class Funcionario(Base):
     __tablename__ = "funcionarios"
@@ -27,10 +26,21 @@ class Funcionario(Base):
     cargo = Column(String(100), nullable=False)
     salario = Column(Float, nullable=False)
 
+    departamento_id = Column(Integer, ForeignKey("departamentos.id"))
+
+    #Relacionamento
+    departamento = relationship("Departamento", back_populates="funcionarios")
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    #Função para imprimir
+    def __repr__(self):
+        return f"Departamento =  id={self.id} - nome={self.nome}"
 
 
-    funcionario_id = Column(Integer,ForeignKey("funcionarios_id"))
+engine = create_engine("sqlite:///empresa.db")
 
+Base.metadata.create_all(engine)
 
-    
-    
+Session = sessionmaker(bind=engine)
