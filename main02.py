9862c50ae9068ba6046daf77a3057bbdaee2b6ef
paulo <pauloricardo.st1812@hotmai.com>
2026-03-20 16:05:39 -0300

    class departamento

--- main02.py
+++ main02.py
@@ -5,4 +5,15 @@ from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 Base = declarative_base()
 
 
-class 
+class Departamento(Base):
+    __tablename__ = "departamentos"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def __repr__(self):
+        return f"Usuario = id={self.id} - nome={self.nome}"
+
